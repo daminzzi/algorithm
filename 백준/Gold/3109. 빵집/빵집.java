33af2898e713af24c61498a42ee05b13294c2771
daminzzi <106113850+daminzzi@users.noreply.github.com>
2023-08-16 15:22:58 +0900

    [Gold II] Title: 빵집, Time: 308 ms, Memory: 46916 KB -BaekjoonHub

---
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int R, C, cnt;
+	static char[][] field;
+	
+	static int[] direction = {-1, 0, 1};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		
+		field = new char[R][C];
+		
+		for(int i = 0; i<R; i++) {
+			field[i] = br.readLine().toCharArray();
+		}
+		cnt = 0;
+		
+		for(int i = 0; i<R; i++) {
+			field[i][0] = '-';
+			dfs(i, 0);
+		}
+		
+		System.out.println(cnt);
+	}
+	
+	static boolean isPossible(int nr, int nc) {
+		if(nr < 0 || nr>= R || nc <0 || nc>=C) {
+			return false;
+		}
+		
+		if(field[nr][nc] != '.') {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	static boolean dfs(int r, int c) {		
+		if(c == C-1) {
+			cnt++;
+			return true;
+		}
+		
+		int nr, nc;
+		
+		for(int i = 0; i<3; i++) {
+			nr = r+direction[i];
+			nc = c+1;
+			if(!isPossible(nr, nc)) {
+				continue;
+			}
+			field[nr][nc] = '-';
+			if(dfs(nr, nc)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+}
