e9cb1eb9a8b0e8822e34da12e05462fe5eb6b74f
daminzzi <106113850+daminzzi@users.noreply.github.com>
2023-08-17 15:41:02 +0900

    [Gold II] Title: 빵집, Time: 296 ms, Memory: 46856 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -1,13 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	static int R, C, cnt;
 	static char[][] field;
-	
 	static int[] direction = {-1, 0, 1};
 	
 	public static void main(String[] args) throws IOException {
@@ -24,6 +24,7 @@ public class Main {
 		}
 		cnt = 0;
 		
+//모든 행에 대해서 탐색을 진행한다.
 		for(int i = 0; i<R; i++) {
 			dfs(i, 0);
 		}
@@ -32,7 +33,8 @@ public class Main {
 	}
 	
 	static boolean dfs(int r, int c) {		
-		if(c == C-1) {
+		//제일 끝 행에 도달한 경우
+		if(c == C-2) {
 			cnt++;
 			return true;
 		}
