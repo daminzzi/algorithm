9b3e504ef7004e67c80106be1aebb26ec3475131
daminzzi <106113850+daminzzi@users.noreply.github.com>
2023-12-13 00:49:50 +0900

    [Gold III] Title: 세 용액, Time: 352 ms, Memory: 161128 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -5,23 +5,23 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static Long[] solution;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        solution = new Long[N];
+        Long[] solution = new Long[N];
         st = new StringTokenizer(br.readLine());
 
-        for(int i = 0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             solution[i] = Long.parseLong(st.nextToken());
         }
 
         Arrays.sort(solution);
 
-        if(N == 3){
-            for(int i = 0; i<N; i++){
+        if (N == 3) {
+            for (int i = 0; i < N; i++) {
                 sb.append(solution[i]).append(' ');
             }
             System.out.println(sb);
@@ -29,36 +29,44 @@ public class Main {
         }
 
         Long comp = Long.MAX_VALUE;
-        int ans1 = 0;
-        int ans2 = 0;
-        int ans3 = 0;
-        for(int i = 0; i<N-2; i++){ //시작 수
-            int lo = i+1;
-            int hi = N-1;
-            Long temp;
-            while(lo < hi){
-                temp = Long.valueOf(solution[i]) + solution[lo] + solution[hi];
+        int ans1 = 0, ans2 = 0, ans3 = 0;
+        if (solution[0] >= 0) {
+            ans1 = 0;
+            ans2 = 1;
+            ans3 = 2;
+        } else if (solution[N - 1] <= 0) {
+            ans1 = N - 3;
+            ans2 = N - 2;
+            ans3 = N - 1;
+        } else {
+            for (int i = 0; i < N - 2; i++) { //시작 수
+                int lo = i + 1;
+                int hi = N - 1;
+                Long temp;
+                while (lo < hi) {
+                    temp = solution[i] + solution[lo] + solution[hi];
 
-                if(temp == 0){
-                    System.out.println(solution[i]+" "+solution[lo]+" "+solution[hi]);
-                    return;
-                }
+                    if (temp == 0) {
+                        System.out.println(solution[i] + " " + solution[lo] + " " + solution[hi]);
+                        return;
+                    }
 
-                if(Math.abs(temp) < comp){
-                    ans1 = i;
-                    ans2 = lo;
-                    ans3 = hi;
-                    comp = Math.abs(Math.abs(temp));
-                }
+                    if (Math.abs(temp) < comp) {
+                        ans1 = i;
+                        ans2 = lo;
+                        ans3 = hi;
+                        comp = Math.abs(Math.abs(temp));
+                    }
 
-                if(temp < 0){
-                    lo++;
-                } else{
-                    hi--;
+                    if (temp < 0) {
+                        lo++;
+                    } else {
+                        hi--;
+                    }
                 }
             }
         }
 
-        System.out.println(solution[ans1]+" "+solution[ans2]+" "+solution[ans3]);
+        System.out.println(solution[ans1] + " " + solution[ans2] + " " + solution[ans3]);
     }
 }
