20d0a3e464fbd19cef62697ca8fa078e63351150
daminzzi <106113850+daminzzi@users.noreply.github.com>
2023-12-14 23:25:43 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2580 ms, Memory: 439256 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,69 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main {
-
-    static Map<Integer, Integer> map;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        while (T-- > 0) {
+        while(T-- > 0){
             int K = Integer.parseInt(br.readLine());
 
-            Queue<Integer> minHeap = new PriorityQueue<>();
-            Queue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-            map = new HashMap<>();
-
-            while (K-- > 0) {
+            TreeMap<Integer, Integer> map = new TreeMap<>();
+            while(K-- > 0){
                 st = new StringTokenizer(br.readLine());
                 char action = st.nextToken().charAt(0);
                 int num = Integer.parseInt(st.nextToken());
 
-                if (action == 'I') {
-                    maxHeap.add(num);
-                    minHeap.add(num);
+                if(action == 'I'){
                     map.put(num, map.getOrDefault(num, 0)+1);
                 }else{
                     if(map.size()==0) continue;
+                    int deleteNum;
                     if(num == 1){
-                        delete(maxHeap);
+                        deleteNum = map.lastKey();
                     }else{
-                        delete(minHeap);
+                        deleteNum = map.firstKey();
+                    }
+                    if(map.put(deleteNum, map.get(deleteNum)-1) == 1){
+                        map.remove(deleteNum);
                     }
                 }
             }
             if(map.size()==0){
                 sb.append("EMPTY\n");
             }else{
-                int res = delete(maxHeap);
-                sb.append(res+" ");
-                if(map.size() > 0){
-                    res = delete(minHeap);
-                }
-                sb.append(res+"\n");
+                sb.append(map.lastKey()+" "+map.firstKey()+"\n");
             }
         }
         System.out.println(sb);
-    }
-
-    static int delete(Queue<Integer> q) {
-        int res = 0;
-        while (true) {
-            res = q.poll();
-
-            int cnt = map.getOrDefault(res, 0);
-            if (cnt == 0) continue;
-
-            if (cnt == 1) map.remove(res);
-            else map.put(res, cnt - 1);
-            break;
-        }
-
-        return res;
     }
 }
