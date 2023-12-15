ccdefe4f6280cd746f8a3d3a87868fcba8b93043
daminzzi <106113850+daminzzi@users.noreply.github.com>
2023-12-15 14:32:26 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 312 ms, Memory: 26020 KB -BaekjoonHub

---
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] lis;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] line = new int[N];
+		lis = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i<N; i++) {
+			line[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int len = 0; // 최장증가 부분 수열 길이
+		int idx = 0; // 삽입 위치
+		for(int i = 0; i<N; i++) {
+			if(line[i] > lis[len]) {
+				len++;
+				lis[len] = line[i];
+				continue;
+			}
+			
+			idx = lower_bound(0, len, line[i]);
+			lis[idx] = line[i];
+		}
+		
+		System.out.println(N - len);
+	}
+	
+	static int lower_bound(int left, int right, int key) {
+		while(left<right) {
+			int mid = (left+right)/2;
+			if(lis[mid]> key) {
+				right = mid;
+			}else {
+				left = mid+1;
+			}
+		}
+		return right;
+	}
+}
