b5b10c20a10ab5883e3f36c9bced2570d90b4409
daminzzi <106113850+daminzzi@users.noreply.github.com>
2024-03-03 15:45:55 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 6.02 ms, Memory: 96.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] queue1, int[] queue2) {
+        int answer = -2;
+        int len = queue1.length;
+        int total = len*2;
+        int[] queue = new int[total];
+        System.arraycopy(queue1, 0, queue, 0, len);
+        System.arraycopy(queue2, 0, queue, len, len);
+        long sum = 0;
+        long target = 0;
+        long temp = 0;
+        for(int i = 0; i<len; i++){
+            sum += queue1[i]+queue2[i];
+            temp += queue1[i];
+        }
+        if(sum % 2 == 1){
+            return -1;
+        }
+        target = sum/2;
+        
+        int idx1 = 0;
+        int idx2 = len;
+        
+        answer = 0;
+        while(answer <= len*4){
+            if(temp == target) {
+                return answer;
+            }
+            if(temp < target) {
+                temp += queue[idx2++];
+                idx2 %= total;
+                answer += 1;
+            }
+            else if(temp > target){
+                temp -= queue[idx1++];
+                idx1 %= total;
+                answer += 1;
+            }
+        }
+        
+        return -1;
+    }
+}
