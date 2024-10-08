c687fb0fb7042b1a85fe5eb1f6028017ff9d73d3
daminzzi <106113850+daminzzi@users.noreply.github.com>
2024-10-08 13:51:20 +0900

    [level 0] Title: 수 조작하기 2, Time: 10.49 ms, Memory: 40.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,15 @@
+function solution(numLog) {
+    var answer = '';
+    const control = {
+        '1': 'w',
+        '-1': 's',
+        '10': 'd',
+        '-10': 'a',
+    }
+    
+    for(i = 1; i<numLog.length; i++){
+        answer += control[numLog[i]-numLog[i-1]];
+    }
+    
+    return answer;
+}
